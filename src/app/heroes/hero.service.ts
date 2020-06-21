c957ecb2ab078811bce12ccd1a328af37e94acca
Morgan Galvin <morgan@ludwigthepig.com>
2020-06-20 23:06:55 -0600

    rfc: move service to heroes dir

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { myHeroes } from './myHeroes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  getHeroes() {
+    return myHeroes;
+  }
 }
